4d7fee3e70cfe0b4cf700975450dd2582668629c
Seliwange <yul.yaremchuk@gmail.com>
2021-09-27 19:36:40 +0300

    feat: add better solution

--- 02-loops-homework/script.js
+++ 02-loops-homework/script.js
@@ -2,24 +2,36 @@ let getNumberN;
 let getNumberM;
 
 do{
-    getNumberN = +prompt("Put number N", "");
+    getNumberN = +prompt("Put integer number N", "");
 }while(!Number.isInteger(getNumberN));
 
 do{
-    getNumberM = +prompt("Put number M", "");
-}while(!Number.isInteger(getNumberM));
+    getNumberM = +prompt("Put integer number M, gteater than N", "");
+}while(!Number.isInteger(getNumberM) || (getNumberM < getNumberN));
 
 
 const getEvenNumber = confirm("Do you want to skip even numbers?");
 
 let sum = 0;
 for(let i = getNumberN; i <= getNumberM; i++){
-    if(getEvenNumber === true){
+    if(getEvenNumber){ // the same as if(getEvenNumber === true)
         if(i % 2 !== 0){
             sum += i;
         }
     } else {
         sum += i;
     }
+
+    /* better
+    if(!getEvenNumber || i % 2){
+        sum += i;
+    }
+    or
+    if(getEvenNumber && (i % 2 === 0)){
+        continue;
+    } else {
+        sum += i;
+    }
+    */
 }
-console.log(`Total sum: ${sum}`);
+document.writeln(`Total sum: ${sum}`);
