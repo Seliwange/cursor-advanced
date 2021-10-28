e8bc478c15a3661a6436e0cbd958af970108dcab
Seliwange <yul.yaremchuk@gmail.com>
2021-10-28 17:22:14 +0300

    fix: change function and styles

--- 11-event-homework/script.js
+++ 11-event-homework/script.js
@@ -1,37 +1,33 @@
 function playAudio(e){
     const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
-    if(!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add("playing");
-}
-
-function playAudioSmall(e){
-    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+    const keyBig = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
     const keySmall = document.querySelector(`.key__small[data-key = "${e.keyCode}"]`);
     if(!audio) return;
     audio.currentTime = 0;
     audio.play();
-    keySmall.classList.add("playing__small");
-    e.stopPropagation();
+    if(keyBig){
+        keyBig.classList.add("playing");
+    } else {
+        e.stopPropagation();
+        keySmall.classList.add("playing__small");
+    }
 }
 
-function removeTransition(e){
+function removeTransitionBigKeys(e){
     if(e.propertyName !== "transform") return;
     this.classList.remove("playing"); 
 }
 
-function removeTransitionSmall(e){
+function removeTransitionSmallKeys(e){
+    e.stopPropagation();
     if(e.propertyName !== "transform") return;
-    this.classList.remove("playing__small"); 
+    this.classList.remove("playing__small");
 }
 
-const keys = document.querySelectorAll(".key");
-keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+const keysBig = document.querySelectorAll(".key");
+keysBig.forEach(key => key.addEventListener("transitionend", removeTransitionBigKeys));
 
 const keysSmall = document.querySelectorAll(".key__small");
-keysSmall.forEach(key => key.addEventListener("transitionend", removeTransitionSmall));
+keysSmall.forEach(key => key.addEventListener("transitionend", removeTransitionSmallKeys));
 
 window.addEventListener('keydown', playAudio);
-window.addEventListener('keydown', playAudioSmall);
